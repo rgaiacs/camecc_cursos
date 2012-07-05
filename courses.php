ea6e7d73f01d71bb2b84c3888e0c427808ed0ebc
Raniere Gaia Silva <raniere@raniere-VGN-SR150A.(none)>
2012-07-04 21:55:45 -0300

    Add list of courses.

--- courses.php
+++ courses.php
@@ -4,11 +4,31 @@ $menu = "<li><a href='index.php' title='Início'>Início</a></li>
 <li class='select'><a href='courses.php' title='Cursos'>Cursos</a></li>
 <li><a href='contact.php' title='Contato'>Contato</a></li>";
 $news = "<li>Anúncio do curso de LaTeX.</li>";
-$main = "<h2>Cursos</h2>
-    Os cursos oferecidos são:
+$main = "<h2>Cursos Existentes</h2>
+    <p>Os cursos a seguir já foram ministrados ao menos uma vez (para novo oferecimento, entre em contato).
     <ul>
     <li>LaTeX</li>
-    </ul>";
+    </ul>
+    </p>
+    <h2>Cursos Em Desenvolvimento</h2>
+    <p>Os cursos a seguir estão em desenvolvimentos e serão oferecidos muito em breve. Caso tenha interesse de contribuir, entre em contato com o responsável.</p>
+    <h2>Futuros Cursos</h2>
+    <p>Os cursos a seguir são aguardados por vários alunos mas ninguém começou a prepará-lo. Se tiver interesse de iniciar algum, entre em contato conosco.
+    <ul>
+    <li>Octave</li>
+    <li>R</li>
+    <li>GeoGebra</li>
+    </ul>
+    </p>
+    <p>Acredita-se que também tenha demanda para os cursos abaixo.
+    <ul>
+    <li>Programação web (html, css, php, javascript, ...)</li>
+    <li>Maxima</li>
+    <li>Python</li>
+    <li>Moodle</li>
+    <li>Vim</li>
+    <li>Emacs</li>
+    </ul></p>";
 
 require('template.php');
 ?>
--- index.php
+++ index.php
@@ -5,8 +5,14 @@ $menu = "<li class='select'><a href='index.php' title='Início'>Início</a></li>
 <li><a href='contact.php' title='Contato'>Contato</a></li>";
 $news = "<li>Anúncio do curso de LaTeX no segundo semestre de 2012.</li>";
 $main = "<h2>Bem vindo.</h2>
-    <p>O CAMECC Cursos é um órgão do CAMECC responsável por manter material de apóio técnico aos alunos do IMECC.</p>
-    <p>Todo o material está disponibilidado sob alguma das licenças <a href='http://creativecommons.org.br/'>Creative Commons</a> para facilitar sua utilização.</p>";
+    <p>O CAMECC Cursos é um órgão do CAMECC responsável por manter material relacionado a softwares livres (normalmente sob a licença GNU GPL) de interesse dos alunos do IMECC.</p>
+    <p>Todo o material está disponibilidado sob uma das seguintes licenças <a href='http://creativecommons.org.br/'>Creative Commons</a>:
+    <ul>
+    <li><a href='http://creativecommons.org/licenses/by/3.0'>Atribuição (CC BY)</a>,</li>
+    <li><a href='http://creativecommons.org/licenses/by-sa/3.0'>Atribuição-CompartilhaIgual (CC BY-SA)</a>,</li>
+    <li><a href='http://creativecommons.org/licenses/by-nc/3.0'>Atribuição-NãoComercial (CC BY-NC)</a>,</li>
+    </ul>
+    no formato pdf (para visualização) e também no código LaTeX/HTML ou arquivo odf (para edição).</p>";
 
 require('template.php');
 ?>
